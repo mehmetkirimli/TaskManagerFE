1dc64511f64650d681879e7a90601a0b49858faa
mehmetkirimli <mehmet.kirimli@vbt.com.tr>
2025-02-19 15:23:26 +0300

    fix+++

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,20 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
+  imports: [ReactiveFormsModule],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+  title = 'TaskManagerFE';
   loginForm: FormGroup;
   registerForm: FormGroup;
 
